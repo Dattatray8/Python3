ee20e5e12f511df7befa9ff7aa3ae3ffb8bf8016
Dattatray Mahindrakar <dattatraymahindrakar@gmail.com>
2025-06-18 20:40:34 +0530

    python script upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a number: "))
+
+def fact(n):
+    if n < 2:
+        return 1
+    else:
+        return n * fact(n-1)
+    
+print(f"Factorial of {n} is: {fact(n)}")
---
+++ task2.py
@@ -0,0 +1,7 @@
+from math import *
+
+num = int(input("Enter a number: "))
+
+print(f"Square root: {sqrt(num)}")
+print(f"Logarithm: {log(num)}")
+print(f"Sine: {sin(num)}")
